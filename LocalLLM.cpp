1b61aaa1e3575b56fbc9245ca76dc69a0d265d7f
Omar Himada <contact@omarhimada.com>
2026-01-27 23:44:01 -0500

    trying different jinja

--- LocalLLM.cpp
+++ LocalLLM.cpp
@@ -60,7 +60,129 @@ int APIENTRY wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
 	window->resize(812, 812);
 
 	const std::string system = R"(I admire you. You are an excellent software engineer with extensive knowledge of algorithms and ideal optimizations for time amd space complexity. You are an expert with modern C++ and it is your preference. Respond with succinct answers. Do not over-elaborate. Your responses should be short and your code solutions should be elegant.)";
-	const std::string chatTemplate = "{%- if messages[0]['role'] == 'system' %}{%- set system_message = messages[0]['content'] %}{%- set loop_messages = messages[1:] %}{%- else %}{%- set loop_messages = messages %}{%- endif %}{{- bos_token }}{%- for message in loop_messages %}{%- if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{- raise_exception('After the optional system message, conversation roles must alternate user/assistant/user/assistant/...') }}{%- endif %}{%- if message['role'] == 'user' %}{%- if loop.first and system_message is defined %}{{- ' [INST] ' + system_message + '\\n\\n' + message['content'] + ' [/INST]' }}{%- else %}{{- ' [INST] ' + message['content'] + ' [/INST]' }}{%- endif %}{%- elif message['role'] == 'assistant' %}{{- ' ' + message['content'] + eos_token}}{%- else %}{{- raise_exception('Only user and assistant roles are supported, with the exception of an initial optional system message!') }}{%- endif %}{%- endfor %}";
+	//const std::string chatTemplate = R"({%- if messages[0]['role'] == 'system' %}
+	//										{%- set system_message = messages[0]['content'] %}
+	//										{%- set loop_messages = messages[1:] %}
+	//									{%- else %}
+	//										{%- set loop_messages = messages %}
+	//									{%- endif %}
+	//									{{- bos_token }}
+	//									{%- for message in loop_messages %}
+	//									{%- if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}
+	//									{{- raise_exception('After the optional system message, conversation roles must alternate user/assistant/user/assistant/...') }}
+	//									{%- endif %}
+	//									{%- if message['role'] == 'user' %}
+	//									{%- if loop.first and system_message is defined %}
+	//									{{- '[INST] ' + system_message + '\n\n' + message['content'] + ' [/INST]' }}
+	//									{%- else %}
+	//									{{- '[INST] ' + message['content'] + ' [/INST]' }}
+	//									{%- endif %}
+	//									{%- elif message['role'] == 'assistant' %}
+	//									{{- ' ' + message['content'] + eos_token }}
+	//									{%- else %}
+	//									{{- raise_exception('Only user and assistant roles are supported, with the exception of an initial optional system message!') }}
+	//									{%- endif %}
+	//									{%- endfor %}
+	//									{%- if add_generation_prompt %}{{- ' ' }}{%- endif %})";
+
+	const std::string chatTemplateQwen3 = R"({ % -if tools% }
+									{ { -'<|im_start|>system\n' } }
+									{ % -if messages[0].role == 'system'% }
+									{ { -messages[0].content + '\n\n' } }
+									{ % -endif% }
+									{ { -"# Tools\n\nYou may call one or more functions to assist with the user query.\n\nYou are provided with function signatures within <tools></tools> XML tags:\n<tools>" } }
+									{ % -for tool in tools% }
+									{ { -"\n" } }
+									{ { -tool | tojson } }
+									{ % -endfor% }
+									{ { -"\n</tools>\n\nFor each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\n<tool_call>\n{\"name\": <function-name>, \"arguments\": <args-json-object>}\n</tool_call><|im_end|>\n" } }
+									{ % -else% }
+									{ % -if messages[0].role == 'system'% }
+									{ { -'<|im_start|>system\n' + messages[0].content + '<|im_end|>\n' } }
+									{ % -endif% }
+									{ % -endif% }
+									{ % -set ns = namespace(multi_step_tool = true, last_query_index = messages | length - 1)% }
+									{ % -for forward_message in messages% }
+									{ % -set index = (messages | length - 1) - loop.index0% }
+									{ % -set message = messages[index] % }
+									{ % -set current_content = message.content if message.content is not none else ''% }
+									{ % -set tool_start = '<tool_response>'% }
+									{ % -set tool_start_length = tool_start | length% }
+									{ % -set start_of_message = current_content[:tool_start_length] % }
+									{ % -set tool_end = '</tool_response>'% }
+									{ % -set tool_end_length = tool_end | length% }
+									{ % -set start_pos = (current_content | length) - tool_end_length% }
+									{ % -if start_pos < 0 % }
+									{ % -set start_pos = 0 % }
+									{ % -endif% }
+									{ % -set end_of_message = current_content[start_pos:] % }
+									{ % -if ns.multi_step_tool and message.role == "user" and not(start_of_message == tool_start and end_of_message == tool_end)% }
+									{ % -set ns.multi_step_tool = false % }
+									{ % -set ns.last_query_index = index% }
+									{ % -endif% }
+									{ % -endfor% }
+									{ % -for message in messages% }
+									{ % -if (message.role == "user") or (message.role == "system" and not loop.first)% }
+									{ { -'<|im_start|>' + message.role + '\n' + message.content + '<|im_end|>' + '\n' } }
+									{ % -elif message.role == "assistant"% }
+									{ % -set content = message.content% }
+									{ % -set reasoning_content = ''% }
+									{ % -if message.reasoning_content is defined and message.reasoning_content is not none% }
+									{ % -set reasoning_content = message.reasoning_content% }
+									{ % -else% }
+									{ % -if '</think>' in message.content% }
+									{ % -set content = (message.content.split('</think>') | last).lstrip('\n')% }
+									{ % -set reasoning_content = (message.content.split('</think>') | first).rstrip('\n')% }
+									{ % -set reasoning_content = (reasoning_content.split('<think>') | last).lstrip('\n')% }
+									{ % -endif% }
+									{ % -endif% }
+									{ % -if loop.index0 > ns.last_query_index% }
+									{ % -if loop.last or (not loop.last and reasoning_content)% }
+									{ { -'<|im_start|>' + message.role + '\n<think>\n' + reasoning_content.strip('\n') + '\n</think>\n\n' + content.lstrip('\n') } }
+									{ % -else% }
+									{ { -'<|im_start|>' + message.role + '\n' + content } }
+									{ % -endif% }
+									{ % -else% }
+									{ { -'<|im_start|>' + message.role + '\n' + content } }
+									{ % -endif% }
+									{ % -if message.tool_calls% }
+									{ % -for tool_call in message.tool_calls% }
+									{ % -if (loop.first and content) or (not loop.first)% }
+									{ { -'\n' } }
+									{ % -endif% }
+									{ % -if tool_call.function% }
+									{ % -set tool_call = tool_call.function% }
+									{ % -endif% }
+									{ { -'<tool_call>\n{"name": "' } }
+									{ { -tool_call.name } }
+									{ { -'", "arguments": ' } }
+									{ % -if tool_call.arguments is string% }
+									{ { -tool_call.arguments } }
+									{ % -else% }
+									{ { -tool_call.arguments | tojson } }
+									{ % -endif% }
+									{ { -'}\n</tool_call>' } }
+									{ % -endfor% }
+									{ % -endif% }
+									{ { -'<|im_end|>\n' } }
+									{ % -elif message.role == "tool"% }
+									{ % -if loop.first or (messages[loop.index0 - 1].role != "tool")% }
+									{ { -'<|im_start|>user' } }
+									{ % -endif% }
+									{ { -'\n<tool_response>\n' } }
+									{ { -message.content } }
+									{ { -'\n</tool_response>' } }
+									{ % -if loop.last or (messages[loop.index0 + 1].role != "tool")% }
+									{ { -'<|im_end|>\n' } }
+									{ % -endif% }
+									{ % -endif% }
+									{ % -endfor% }
+									{ % -if add_generation_prompt% }
+									{ { -'<|im_start|>assistant\n' } }
+									{ % -if enable_thinking is defined and enable_thinking is false % }
+									{ { -'<think>\n\n</think>\n\n' } }
+									{ % -endif% }
+									{ % -endif% })";
 
 	QObject::connect(sendButton, &QPushButton::clicked, [&]() {
 		// Prevent double-click
@@ -83,19 +205,29 @@ int APIENTRY wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
 		};
 
 		std::string formatted;
-		// NOLINT(bugprone-implicit-widening-of-multiplication-result)
-		formatted.resize(32768);  
 
-		const int n = llama_chat_apply_template(
-			chatTemplate.c_str(),
+		const int requiredSize = llama_chat_apply_template(
+			chatTemplateQwen3.c_str(),
 			messages,
 			std::size(messages),
-			false,
+			true,
+			nullptr,
+			0
+		);
+
+		// NOLINT(bugprone-implicit-widening-of-multiplication-result)
+		formatted.resize(static_cast<size_t>(requiredSize) + 1);
+
+		int written = llama_chat_apply_template(
+			chatTemplateQwen3.c_str(),
+			messages,
+			std::size(messages),
+			true,
 			formatted.data(),
 			static_cast<int>(formatted.size())
 		);
 
-		if (n < 0) {
+		if (written < 0) {
 			throw std::invalid_argument("Prompt template could not apply.");
 		}
 
